7cbe8a632e5cc52219c2bbee5bb6e2250fc4dd53
nikolaisorokin <59018323+nikolaisorokin@users.noreply.github.com>
2021-10-03 17:21:00 +0300

    (lab3) wrote a function for user input

--- lab3/main.c
+++ lab3/main.c
@@ -1,23 +1,27 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+int InputNumber();
 
 int main() {
   
-    int octal_number;
-
-    int given_input = scanf("%i", &octal_number);
-    if (given_input != 1) { return 1; }
+    int octal_number = InputNumber();
 
     printf("ex.2 (decimal): %d\n", octal_number);
     printf("ex.3 0x%x >> 2 = 0x%x\n", octal_number, octal_number >> 2);
     printf("ex.4 ~0x%x = 0x%x\n", octal_number, ~octal_number);
     
-    int hex_number;
+    int hex_number = InputNumber();
 
-    given_input = scanf("%i", &hex_number);
-    if (given_input != 1) { return 1; }
-    
     printf("ex.5 0x%x | 0o%o = 0x%x\n", hex_number, octal_number,
            hex_number | octal_number);
     
     return 0;
+}
+
+int InputNumber() {
+    int number;
+    if (scanf("%i", &number) != 1) { exit(1); }
+    
+    return number;
 }

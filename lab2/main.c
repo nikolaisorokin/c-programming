478867ea1443a712ea858877c090d3ab6bfd5e90
nikolaisorokin <59018323+nikolaisorokin@users.noreply.github.com>
2021-10-03 14:30:18 +0300

    lab2 completed

---
+++ lab2/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    double alpha;
+    
+    int given_input = scanf("%lf", &alpha);
+
+    if (given_input != 1) { return 1; }
+
+    double z1 = cos(alpha) + sin(alpha) + cos(3 * alpha) + sin(3 * alpha);
+    double z2 = 2 * sqrt(2) * cos(alpha) * sin(M_PI / 4 + 2 * alpha);
+    
+    printf("%.3lf, %.3lf\n", z1, z2);
+ 
+     return 0;
+}
+

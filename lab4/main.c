5532726c59464df686cef08e158d7735e0ec5567
nikolaisorokin <59018323+nikolaisorokin@users.noreply.github.com>
2021-11-04 22:46:29 +0300

    minor fixes

--- lab4/main.c
+++ lab4/main.c
@@ -1,34 +1,23 @@
 #include <stdio.h>
-#include <stdlib.h>
 
-int InputNumber();
 int IsInRange(int, int, int);
 
 int main() {
-   int number = InputNumber();
+   int number;
+   scanf("%d", &number);
 
-   int lhs = 100;
-   int rhs = 1000;
+   int left = 100;
+   int right = 1000;
 
-   printf("is %d in [%d-%d]? (1 - yes, 0 - no): %d\n", number, lhs, rhs,
-          IsInRange(lhs, rhs, number));
+   printf("is %d in [%d-%d]? (1 - yes, 0 - no): %d\n", number, left, right,
+          IsInRange(left, right, number));
    
-   int second_number = InputNumber();
-
-   printf("%d\n", (second_number & (1 << 22)) >> 22);
+   scanf("%x", &number);
+   printf("%d\n", (number & (1 << 22)) >> 22);
 
    return 0;
 }
 
-int InputNumber() {
-    int number;
-    
-    if (scanf("%i", &number) != 1) { exit(1); }
-
-    return number;
+int IsInRange(int left, int right, int number) {
+    return (number >= left && number <= right);
 }
-
-int IsInRange(int lhs, int rhs, int number) {
-    return (number >= lhs && number <= rhs);
-}
-

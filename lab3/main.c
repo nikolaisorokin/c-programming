59cfdcdf2f7e172396268b8cf03086d637f97047
nikolaisorokin <59018323+nikolaisorokin@users.noreply.github.com>
2021-11-04 22:51:58 +0300

    removed unnecessary function

--- lab3/main.c
+++ lab3/main.c
@@ -1,27 +1,19 @@
 #include <stdio.h>
-#include <stdlib.h>
-
-int InputNumber();
 
 int main() {
   
-    int octal_number = InputNumber();
+    int octal_number;
+    scanf("%o", &octal_number);
 
     printf("ex.2 (decimal): %d\n", octal_number);
     printf("ex.3 0x%x >> 2 = 0x%x\n", octal_number, octal_number >> 2);
     printf("ex.4 ~0x%x = 0x%x\n", octal_number, ~octal_number);
     
-    int hex_number = InputNumber();
+    int hex_number;
+    scanf("%x", &hex_number);
 
     printf("ex.5 0x%x | 0o%o = 0x%x\n", hex_number, octal_number,
            hex_number | octal_number);
     
     return 0;
-}
-
-int InputNumber() {
-    int number;
-    if (scanf("%i", &number) != 1) { exit(1); }
-    
-    return number;
 }

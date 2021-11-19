4cc03dfafb7b53290c4124cd583b1d37cf8ab4de
nikolaisorokin <59018323+nikolaisorokin@users.noreply.github.com>
2021-11-19 06:49:45 +0300

    functions for array initialization added

--- lab10/main.c
+++ lab10/main.c
@@ -2,43 +2,44 @@
 #include <stdlib.h>
 #include <math.h>
 
-typedef struct {
+struct Point {
     double x, y;
-} Point;
+};
 
 int LengthOf(int);
-double Distance(Point, Point);
-void InitArray(int*, int);
+double Distance(struct Point, struct Point);
+void InitArray(int**, int*, int);
 void PrintArray(int*);
-void Init2dArray(double*, int, int, Point*);
+void Init2dArray(double**, int, int, struct Point*);
 
 int main() {
     // (3)
     int number;
     scanf("%d", &number);
 
-    int size = LengthOf(number);
-    int arr[size];
-    int *p = arr;
+    int size;
+    int* arr;
 
-    InitArray(p, number);
-    PrintArray(p);
+    InitArray(&arr, &size, number);
+    PrintArray(arr);
+
+    free(arr);
 
     // (2)
     int n_points;
     scanf("%d", &n_points);
     int rows = n_points, cols = n_points - 1;
-    double *distances;
-    distances = malloc(rows * cols * sizeof (*distances));
 
-    Point points[n_points];
-    Point *ptr = points;
+    struct Point points[n_points];
+    struct Point *ptr = points;
 
     for (int i = 0; i < n_points; ++i) {
         scanf("%lf%lf", &(ptr + i)->x, &(ptr + i)->y);
     }
 
-    Init2dArray(distances, rows, cols, ptr);
+    double *distances;
+    Init2dArray(&distances, rows, cols, ptr);
+
     for (int i = 0; i < rows; ++i) {
         for (int j = 0; j < cols; ++j) {
             printf("%lf ", distances[i*cols+j]);
@@ -62,12 +63,20 @@ int LengthOf(int number) {
     return length;
 }
 
-void InitArray(int* p, int number) {
+void InitArray(int** arr, int* size, int number) {
+    int length = LengthOf(number);
+    *size = length;
+
+    int *p = malloc(sizeof(int) * *size);
+
+    int i = 0;
     while (number) {
-        *p = number % 10;
+        p[i] = number % 10;
         number /= 10;
-        ++p;
+        ++i;
     }
+
+    *arr = p;
 }
 
 void PrintArray(int* p) {
@@ -78,18 +87,21 @@ void PrintArray(int* p) {
     putchar('\n');
 }
 
-double Distance(Point first, Point second) {
+double Distance(struct Point first, struct Point second) {
     return sqrt(pow((first.x - second.x), 2) + pow((first.y - second.y), 2));
 }
 
-void Init2dArray(double* arr, int rows, int cols, Point* points) {
+void Init2dArray(double** arr, int rows, int cols, struct Point* points) {
+    double* p = malloc(rows * cols * sizeof (*p));
+
     for (int i = 0; i < rows; ++i) {
         int it = 0;
         for (int j = 0; j < cols; ++j) {
             if (j == it && it == i) ++it;
-            Point a = *(points + i);
-            Point b = *(points + it++);
-            arr[i*cols+j] = Distance(a, b);
+            struct Point a = *(points + i);
+            struct Point b = *(points + it++);
+            p[i*cols+j] = Distance(a, b);
         }
     }
+    *arr = p;
 }
